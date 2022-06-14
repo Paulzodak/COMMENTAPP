7a3f5c88ca26566841cfc6f58fae43f26b66d872
OJE PAUL OLUWAFERANMI <ojepauloluwaferanmi@OJEs-MacBook-Pro.local>
2022-06-14 09:35:22 +0100

    no message

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,18 +3,81 @@ import Commentitem from "./components/commentitem/commentitem.js";
 import Addcomment from "./components/addcomment/addcomment";
 import Card from "./components/commentitem/card";
 
+// -------IMAGES PNGS---------
+import amyrobson from "./images/avatars/image-amyrobson.png";
+import juliusomo from "./images/avatars/image-juliusomo.png";
+import maxblagus from "./images/avatars/image-maxblagun.png";
+import ramsesmiron from "./images/avatars/image-ramsesmiron.png";
+
+// --------IMAGES WEBP--------
+import amyrobsonwebp from "./images/avatars/image-amyrobson.webp";
+import juliusomowebp from "./images/avatars/image-juliusomo.webp";
+import maxblaguswebp from "./images/avatars/image-maxblagun.webp";
+import ramsesmironwebp from "./images/avatars/image-ramsesmiron.webp";
+
 function App() {
+  const comments = [
+    {
+      id: 1,
+      content:
+        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: amyrobson,
+          webp: amyrobsonwebp,
+        },
+        username: "amyrobson",
+      },
+      replies: [],
+    },
+    {
+      id: 2,
+      content:
+        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      createdAt: "2 weeks ago",
+      score: 5,
+      user: {
+        image: {
+          png: maxblagus,
+          webp: maxblagus.webp,
+        },
+        username: "maxblagun",
+      },
+      replies: [
+        {
+          id: 3,
+          content:
+            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          createdAt: "1 week ago",
+          score: 4,
+          replyingTo: "maxblagun",
+          user: {
+            image: {
+              png: ramsesmiron,
+              webp: ramsesmironwebp,
+            },
+            username: "ramsesmiron",
+          },
+        },
+      ],
+    },
+  ];
   return (
     <div className="App">
-      <Card>
-        <Commentitem></Commentitem>
-      </Card>
-      <Card>
-        <Commentitem></Commentitem>
-      </Card>
-      <Card>
-        <Commentitem></Commentitem>
-      </Card>
+      {comments.map((x) => (
+        <Card>
+          <Commentitem
+            message={x.content}
+            score={x.score}
+            username={x.user.username}
+            date={x.createdAt}
+            picturesrc={x.user.image}
+          ></Commentitem>
+        </Card>
+      ))}
+
       <Card>
         <Addcomment></Addcomment>
       </Card>
--- myapp/src/components/commentitem/commentitem.js
+++ myapp/src/components/commentitem/commentitem.js
@@ -1,29 +1,28 @@
+// ---------{IMPORTS}----------
 import "./commentitem.css";
-import data from "../../data.json";
-import amyrobson from "../../images/avatars/image-amyrobson.png";
+
+// --------BTNS---------------
 import plus_btn from "../../images/icon-plus.svg";
 import minus_btn from "../../images/icon-minus.svg";
 import reply_btn from "../../images/icon-reply.svg";
-function Commentitem() {
+
+// ---------{COMPONENT}---------------
+function Commentitem(props) {
   return (
     <div>
       <div className="box1">
-        <img className="Profile_image" src={amyrobson}></img>
-        <p className="name">amyrobsonn</p>
-        <p className="Date"> 1 Month Ago</p>
+        <img className="Profile_image" src={props.picturesrc.png}></img>
+        <p className="name">{props.username}</p>
+        <p className="Date">{props.date}</p>
       </div>
-      <div className="box2">
-        "Impressive! Though it seems the drag feature could be improved. But
-        overall it looks incredible. You've nailed the design and the
-        responsiveness at various breakpoints works really well."
-      </div>
+      <div className="box2">{props.message}</div>
       <div className="box3">
         {/* ---UTILITY BTNS --- */}
         <div className="utility_btns">
           <div className="plus">
             <img src={plus_btn} />
           </div>
-          <div className="number"> 12</div>
+          <div className="number">{props.score}</div>
           <div className="minus">
             <img src={minus_btn} />
           </div>
